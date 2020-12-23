7a6575e61ae218398b235b219d593de459d6bea5
MaxenceLenoir <maxence.lenoir1206@gmail.com>
2020-12-23 15:36:39 +0100

    edit + patch method finished

--- client/src/actions/index.jsx
+++ client/src/actions/index.jsx
@@ -7,6 +7,7 @@ import {
   EDIT_STREAM,
   DELETE_STREAM
 } from './types'
+import history from '../history';
 import streams from '../apis/stream'
 
 export const signIn = (userId) => {
@@ -26,7 +27,8 @@ export const createStream = formValues => async (dispatch, getState) => {
   const { userId } = getState().auth;
   const response = await streams.post('/streams', { ...formValues, userId } );
 
-  dispatch({ type: CREATE_STREAM, payload: response.data})
+  dispatch({ type: CREATE_STREAM, payload: response.data});
+  history.push('/')
 }
 
 export const fetchStreams = () => async dispatch => {
@@ -42,9 +44,10 @@ export const fetchStream = (id) => async dispatch => {
 }
 
 export const editStream = (id, formValues) => async dispatch => {
-  const response = await streams.put(`/streams/${id}`, formValues);
+  const response = await streams.patch(`/streams/${id}`, formValues);
 
   dispatch({ type: EDIT_STREAM, payload: response.data})
+  history.push('/')
 }
 
 export const deleteStream = (id) => async dispatch => {
